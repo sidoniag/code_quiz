b1e733350bca77f7ea2af3f5a5f074028f42d0f0
idonia <sidoniag@gmail.com>
2020-06-14 23:36:52 -0500

    start java assignments

---
+++ assets/script.js
@@ -0,0 +1,87 @@
+//assignments
+var currentQuestion = 0;
+var timeleft = 60;
+
+//question assignment
+var questionArray = [
+    {
+        question: "",
+        option1: "",
+        option2: "",
+        option3: "",
+        option4: "",
+        correct: ""
+    },
+    {
+        question: "",
+        option1: "",
+        option2: "",
+        option3: "",
+        option4: "",
+        correct: ""
+    },
+    {
+        question: "",
+        option1: "",
+        option2: "",
+        option3: "",
+        option4: "",
+        correct: ""
+    },
+    {
+        question: "",
+        option1: "",
+        option2: "",
+        option3: "",
+        option4: "",
+        correct: ""
+    }, 
+    {
+        question: "",
+        option1: "",
+        option2: "",
+        option3: "",
+        option4: "",
+        correct: ""
+    },
+    {
+        question: "",
+        option1: "",
+        option2: "",
+        option3: "",
+        option4: "",
+        correct: ""
+    },
+    {
+        question: "",
+        option1: "",
+        option2: "",
+        option3: "",
+        option4: "",
+        correct: ""
+    },
+    {
+        question: "",
+        option1: "",
+        option2: "",
+        option3: "",
+        option4: "",
+        correct: ""
+    },
+    {
+        question: "",
+        option1: "",
+        option2: "",
+        option3: "",
+        option4: "",
+        correct: ""
+    },
+    {
+        question: "",
+        option1: "",
+        option2: "",
+        option3: "",
+        option4: "",
+        correct: ""
+    },
+]
